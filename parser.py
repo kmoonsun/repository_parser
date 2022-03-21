3e4fcd94735fda9116ce7273e32b3c17942793e0
kmoonsun <kmoonsun95@gmail.com>
2022-03-21 22:58:19 +0900

    parser update

--- parser.py
+++ parser.py
@@ -1,6 +1,17 @@
 import os, sys
 
-root_dir = sys.argv[1]
+root_directory = sys.argv[1]
 
-for root, dirs, files in os.walk(root_dir):
-    print(root)
+file_list = list()
+for root, dirs, files in os.walk(root_directory):
+    for file in files:
+        ext = os.path.splitext(file)[-1].split('.')[-1]
+        if ext == '':
+            print('none')
+        print(ext)
+        file_list.append(root+'/'+file)    
+
+with open(file_list[0], 'r') as fp:
+    source = fp.read()
+
+print(source)
