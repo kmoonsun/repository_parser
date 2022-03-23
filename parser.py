2ca7f88ef837f829dcb7a39a3af97c91bbd3435a
kmoonsun <kmoonsun95@gmail.com>
2022-03-23 20:42:32 +0900

    add compare module

--- parser.py
+++ parser.py
@@ -1,6 +1,12 @@
-import os, sys, json, re
+import os
+import sys
+import json
+import re
+import itertools
+import numpy as np
 
 KEYWORD_SET = './keywords.json'
+SPDX_LICENSES = ''
 
 class Parser:
     def __init__(self):
@@ -8,15 +14,69 @@ class Parser:
 
     def c_comment_parser(self, source):
         # parse comment
-        comment = re.findall(r'(//[^\n]*|/\*.*?\*/)', source, re.MULTILINE | re.DOTALL)
-        # parse text
-        text = [re.sub('^[/]|[/*]', '', i).replace('\t', '')  for i in comment]
-
+        comment = re.findall(r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"', source, re.MULTILINE | re.DOTALL)
+        # parse text, filtering empty elems 
+        text = list(filter(None, [re.sub(r'\"(.*?)\"|\'(.*?)\'|[/*]|[\t\n]|  ', '', i).strip() for i in comment]))
         return text
 
-    def shell_comment_parser(self, source):
+    def spdx_license_loader(self, spdx):
         pass
 
+class Tokenizer:
+    def __init__(self):
+        pass
+
+    def get_grams(self, comments):
+        grams = list()
+        for comment in comments:
+            # remove whitespace (2 more)
+            comment = re.sub(r'\s\s+', ' ', comment)
+            words = comment.split(' ')
+            grams.extend(list(zip(words, words[1:])))
+        
+        return grams
+
+class TokenComapre:
+    def __init__(self):
+        pass
+
+    def lcs_lens(self, xs, ys):
+        curr = list(itertools.repeat(0, 1 + len(ys)))
+        for x in xs:
+            prev = list(curr)
+            for i, y in enumerate(ys):
+                if x == y:
+                    curr[i + 1] = prev[i] + 1
+                else:
+                    curr[i + 1] = max(curr[i], prev[i + 1])
+        return curr
+
+    def lcs(self, xs, ys):
+        nx, ny = len(xs), len(ys)
+        if nx == 0:
+            return []
+        elif nx == 1:
+            return [xs[0]] if xs[0] in ys else []
+        else:
+            i = nx // 2
+            xb, xe = xs[:i], xs[i:]
+            ll_b = self.lcs_lens(xb, ys)
+            ll_e = self.lcs_lens(xe[::-1], ys[::-1])
+            _, k = max((ll_b[j] + ll_e[ny - j], j)
+                        for j in range(ny + 1))
+            yb, ye = ys[:k], ys[k:]
+
+            return self.lcs(xb, yb) + self.lcs(xe, ye)
+
+    def lcs_similarity(self, xs, ys):
+        output = self.lcs(xs, ys)
+
+        try:
+            simularity = len(output) / max(len(xs), len(ys))
+        except:
+            simularity = 0
+
+        return simularity
 
 def main():
     root_directory = sys.argv[1]
@@ -36,12 +96,14 @@ def main():
     parser = Parser()
     blocks = parser.c_comment_parser(source)
 
-    print()
-    for idx, i in enumerate(blocks):
-        print(i)
+    tokenizer = Tokenizer()
+    grams = tokenizer.get_grams(blocks)
 
-    #print(source)
-    print()
+    token_compare = TokenComapre()
+    lcs_score = token_compare.lcs_similarity(grams, grams)
+
+    print(lcs_score)
+
 
 
 if __name__ == '__main__':
